8e5ac4a30c9218a9f56cd88be850930d96c3106b
supersand <supersand96@gmail.com>
2017-05-19 10:52:58 +0200

    Add files via upload

---
+++ ipa_2017/src/ipa_2017/Csv.java
@@ -0,0 +1,47 @@
+package ipa_2017;
+
+import java.sql.*;
+
+public class Csv
+{
+	public static void main (String[] args)
+	{
+		try
+		{
+	        //Baut Verbindung mit der SQL-Datenbank auf
+			String driver = "com.mysql.jdbc.Driver";
+			String url = "jdbc:mysql://192.168.25.97:3306/log_db";
+	        Class.forName(driver);
+			Connection conn = DriverManager.getConnection(url,"csv","ipa2017");
+	        Statement st = conn.createStatement();
+
+	        //Erstellen der SQL-Abfrage in einem String
+	        String query = "SELECT * FROM log_db.logfiles";
+
+	        //Resultate der SQL-Abfrage werden im ResultSet gespeichert
+	        ResultSet rs = st.executeQuery(query);
+
+	        //Iterative Schleife geht durch das ResultSet
+	        while (rs.next())
+	        {
+	          int id = rs.getInt("id");
+	          String Var1 = rs.getString("Var1");
+	          String TimeString = rs.getString("TimeString");
+	          String MsgText = rs.getString("MsgText");
+	          String PLC = rs.getString("PLC");
+
+	          //Ausgabe der Resultate im Konsolenfenster
+		      System.out.format("%s, %s, %s, %s, %s \n", id, Var1, TimeString, MsgText, PLC);
+	        }
+
+	        System.out.print("Successful!");
+	        conn.close();
+	     }
+	       	catch (Exception e)
+	       	{
+	       		System.err.println("Exception caught:");
+	       		System.err.println(e.getMessage());
+	       	}
+
+	}
+}
